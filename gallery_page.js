7cd3b01d5fc4c03cca410c0347b3d81ecc47a50b
DannyENL <72445029+DannyENL@users.noreply.github.com>
2021-01-18 12:14:05 +0000

    Comments added, placeholder text replaced

--- gallery_page.js
+++ gallery_page.js
@@ -1,20 +1,21 @@
-function gallery_im_enlarge() {
-	var list_of_ims = document.getElementsByName("gallery_im");
+//Used on the Gallery page to expand images when clicked on
+function gallery_im_enlarge() { //Triggered on page load
+	var list_of_ims = document.getElementsByName("gallery_im"); //Finds all the gallery thumbnail images
 	for (var i = 0; i < list_of_ims.length; i++) {
-		list_of_ims[i].addEventListener("click", function(){enable_overlay()});
+		list_of_ims[i].addEventListener("click", function(){enable_overlay()}); //Adds an event listener. When the image is clicked it'll trigger enable_overlay
 	}
 
-	function enable_overlay() {
-		document.getElementById("big_photo").src=event.target.src;
-		document.getElementById("big_photo").style.display="inline";
-		document.getElementById("overlay").style.display="inline";
-		document.getElementById("overlay").addEventListener("click", disable_overlay);
-		document.getElementById("big_photo").addEventListener("click", disable_overlay);
+	function enable_overlay() { //Triggered when an image is clicked
+		document.getElementById("big_photo").src=event.target.src; //Sets the big photo source to be the chosen image
+		document.getElementById("big_photo").style.display="inline"; //Display is visible
+		document.getElementById("overlay").style.display="inline"; //Display is visible
+		document.getElementById("overlay").addEventListener("click", disable_overlay); //Adds event listener to overlay - when clicked, trigger disable_overlay
+		document.getElementById("big_photo").addEventListener("click", disable_overlay); //Adds event listener to overlay - when clicked, trigger disable_overlay
 		
 	}
 
-	function disable_overlay() {
-		document.getElementById("overlay").style.display="none";
-		document.getElementById("big_photo").style.display="none";
+	function disable_overlay() { //Triggered when the overlay is clicked
+		document.getElementById("overlay").style.display="none"; //Hides the overlay background
+		document.getElementById("big_photo").style.display="none"; //Hides the image
 	}
 }
--- random_image.js
+++ random_image.js
@@ -1,16 +1,16 @@
-//Random images
-var number_of_im = 19 //highest num including 0
-function randomise() { //triggered on page load
-	var im1=Math.floor(Math.random() * number_of_im)
-	var im2=Math.floor(Math.random() * number_of_im)
-	var im3=Math.floor(Math.random() * number_of_im)
-	while (im2 == im1) {
+//Sets the three random images at the bottom of Home and Gallery
+var number_of_im = 19 //Highest numbered file
+function randomise() { //Triggered on page load
+	var im1=Math.floor(Math.random() * number_of_im) //Picks a random number for the first image
+	var im2=Math.floor(Math.random() * number_of_im) //Picks a random number for the second image
+	var im3=Math.floor(Math.random() * number_of_im) //Picks a random number for the third image
+	while (im2 == im1) { //While the first two images are identical, keep selecting a new second image
 		var im2=Math.floor(Math.random() * number_of_im)
 	}
-	while ((im3 == im2) | (im1 == im3)) {
+	while ((im3 == im2) | (im1 == im3)) { //While the third image isn't unique, keep selecting a new third image
 		var im3=Math.floor(Math.random() * number_of_im)
 	}
-	document.getElementById("gallery_im1").src = im1+".jpg";
-	document.getElementById("gallery_im2").src = im2+".jpg";
-	document.getElementById("gallery_im3").src = im3+".jpg";
+	document.getElementById("gallery_im1").src = "images/"+im1+".jpg"; //Set the source for the first image element
+	document.getElementById("gallery_im2").src = "images/"+im2+".jpg"; //Set the source for the second image element
+	document.getElementById("gallery_im3").src = "images/"+im3+".jpg"; //Set the source for the third image element
 }
