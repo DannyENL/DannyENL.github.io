930309b74cc8e2f669b3a92b3514d995eec0dcb5
DannyENL <72445029+DannyENL@users.noreply.github.com>
2021-01-19 09:55:10 +0000

    Validation met, code tidied up

--- gallery_page.js
+++ gallery_page.js
@@ -1,21 +1,21 @@
 //Used on the Gallery page to expand images when clicked on
 function gallery_im_enlarge() { //Triggered on page load
-	var list_of_ims = document.getElementsByClassName("gallery_im"); //Finds all the gallery thumbnail images
-	for (var i = 0; i < list_of_ims.length; i++) {
-		list_of_ims[i].addEventListener("click", function(){enable_overlay()}); //Adds an event listener. When the image is clicked it'll trigger enable_overlay
+	var list_of_ims=document.getElementsByClassName("gallery_im");//Finds all the gallery thumbnail images
+	for (var i=0;i<list_of_ims.length;i++) {
+		list_of_ims[i].addEventListener("click",function(){enable_overlay()});//Adds an event listener. When the image is clicked it'll trigger enable_overlay
 	}
 
 	function enable_overlay() { //Triggered when an image is clicked
-		document.getElementById("big_photo").src=event.target.src; //Sets the big photo source to be the chosen image
-		document.getElementById("big_photo").style.display="inline"; //Display is visible
-		document.getElementById("overlay").style.display="inline"; //Display is visible
-		document.getElementById("overlay").addEventListener("click", disable_overlay); //Adds event listener to overlay - when clicked, trigger disable_overlay
-		document.getElementById("big_photo").addEventListener("click", disable_overlay); //Adds event listener to overlay - when clicked, trigger disable_overlay
+		document.getElementById("big_photo").src=event.target.src;//Sets the big photo source to be the chosen image
+		document.getElementById("big_photo").style.display="inline";//Display is visible
+		document.getElementById("overlay").style.display="inline";//Display is visible
+		document.getElementById("overlay").addEventListener("click",disable_overlay);//Adds event listener to overlay - when clicked, trigger disable_overlay
+		document.getElementById("big_photo").addEventListener("click",disable_overlay);//Adds event listener to overlay - when clicked, trigger disable_overlay
 		
 	}
 
 	function disable_overlay() { //Triggered when the overlay is clicked
-		document.getElementById("overlay").style.display="none"; //Hides the overlay background
-		document.getElementById("big_photo").style.display="none"; //Hides the image
+		document.getElementById("overlay").style.display="none";//Hides the overlay background
+		document.getElementById("big_photo").style.display="none";//Hides the image
 	}
 }
